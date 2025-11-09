fb947ccc0a0a5f77b84824192b770be1a0c2d7f2
ratnawahyuni12 <ratna120307@gmail.com>
2025-11-09 13:54:16 +0700

    Modifikasi percobaan 4 (perintah nomor 2)

--- SearchNilai21.java
+++ SearchNilai21.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 public class SearchNilai21 {
     public static void main(String[] args) {
-        int[] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
-        int hasil = 0;
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int banyakNilai = sc.nextInt();
+
+        int[] arrNilai = new int[banyakNilai];
+
+        for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + ( i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
+
+        int hasil = -1; // apabila belum ditemukan
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
                 hasil = i;
@@ -12,7 +25,13 @@ public class SearchNilai21 {
         }
 
         System.out.println();
-        System.out.println("Nilai " + key + " ketemu di indeks ke-" + hasil);
-        System.out.println();
+
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        } else {
+            System.out.println("Nilai " + key + " tidak ditemukan dalam data.");
+        }
+
+        sc.close();
     }
 }
