03b1fad3023de96ceb0efa79a350e1fe769d733b
ratnawahyuni12 <ratna120307@gmail.com>
2025-11-09 11:16:02 +0700

    Modifikasi percobaan 2 (perintah nomor 4)

--- ArrayNilai21.java
+++ ArrayNilai21.java
@@ -13,6 +13,8 @@ public class ArrayNilai21 {
         for (int i = 0; i < nilaiAkhir.length; i++) {
             if (nilaiAkhir[i] > 70) {
                 System.out.println("Mahasiswa ke-" + i + " lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " tidak lulus!");
             }
         }
 
